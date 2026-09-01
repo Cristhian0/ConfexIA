ab22b41f1f327835fd30fbf78e899f63bdc84bd3
difox11q <difoxsanz@gmail.com>
2026-08-31 22:43:20 -0500

    fix: point frontend to render backend

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://confexia.onrender.com/api/v1'
+  // URL del backend en Render para producción
+};
+
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://confexia.onrender.com/api/v1'
+  // apiUrl: 'http://localhost:8001/api/v1'
+};
+
